8fa99c6303be7f72fad17ca2dc21b780b3b2461f
X702-2025 <mohmmad201048@gmail.com>
2021-10-19 02:51:29 +0300

    hw

---
+++ script.js
@@ -0,0 +1,57 @@
+            //summation\\
+
+// function Summation(N) {
+//     const total = 0;
+//       for(let i = 1; i <= N; i++){
+//         total += i;
+//       }
+//       return total;
+//     }
+// console.log(Summation(15))
+console.log("-----------------------------------------------------")
+            //summationEven\\
+
+// function summationEven(N) {
+//     let total = 0;
+//     for (let i = 0; (i <= 5)&&(i%2==0); i=i+2) {
+//         total += i;
+//     }
+//     return total;
+// }
+// console.log(summationEven())
+console.log("-----------------------------------------------------")
+            //avg\\ 
+// let avg=[8, 2, 2, 4]
+// let total = 0;
+// for(let i = 0; i < avg.length; i++) {
+//     total += avg[i];
+// }
+// let average = total / avg.length;
+// console.log(average)
+console.log("-----------------------------------------------------")
+                    //reverseString\\
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// console.log(reverseString("caterpillar"))
+console.log("-----------------------------------------------------")
+                        //addDashes\\
+
+// const addDashes=(['test1', 'test2', 'test3'])
+//     add= addDashes.join("-")              
+//     console.log(add)
+console.log("-----------------------------------------------------")
+    
+
+//cant figure it out but it seems like it could be done using recursion
+
+
+//const countDownAndUp=i
+console.log("-----------------------------------------------------")
+
+// let words = ['cat', 'rabbit', 'dog', 'frog'];
+// let wordsWitha = words.filter(function(word) {
+//     return word[0] == 'a';
+// });
+// console.log(word)
